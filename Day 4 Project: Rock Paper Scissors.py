d5c57126249186e27602e4673ef89ba2f2f2e761
masoud mashayekh <masoudmashayekh4944@gmail.com>
2024-07-05 00:15:49 +0200

    Update Day 4 Project: Rock Paper Scissors.py

--- Day 4 Project: Rock Paper Scissors.py
+++ Day 4 Project: Rock Paper Scissors.py
@@ -25,30 +25,37 @@ scissors = '''
 ---.__(___)
 '''
 
+#Write your code below this line 👇
 import random
 pc_choice = random.randint(0,2)
 print("Welcome to Rock, Paper, Scissors!")
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
-if user_choice == 0:
-    if pc_choice == 0:
-        print(f"You chose: {rock}\nComputer chose: {rock}" + "\nIt's a draw!")
-    elif pc_choice == 1:
-        print(f"You chose: {rock}\nComputer chose: {paper}" + "\nYou lose!")
-    else:
-        print(f"You chose: {rock}\nComputer chose: {scissors}" + "\nYou win!")
+if 0 <= user_choice <= 2:
+    if user_choice == 0:
+        if pc_choice == 0:
+            print(f"You chose: {rock}\nComputer chose: {rock}" + "\nIt's a draw!")
+        elif pc_choice == 1:
+            print(f"You chose: {rock}\nComputer chose: {paper}" + "\nYou lose!")
+        else:
+            print(f"You chose: {rock}\nComputer chose: {scissors}" + "\nYou win!")
         
-if user_choice == 1:
-    if pc_choice == 1:
-        print(f"You chose: {paper}\nComputer chose: {paper}" + "\nIt's a draw!")
-    elif pc_choice == 0:
-        print(f"You chose: {paper}\nComputer chose: {rock}" + "\nYou win!")
-    else:
-        print(f"You chose: {paper}\nComputer chose: {scissors}" + "\nYou lose!")
+    if user_choice == 1:
+        if pc_choice == 1:
+            print(f"You chose: {paper}\nComputer chose: {paper}" + "\nIt's a draw!")
+        elif pc_choice == 0:
+            print(f"You chose: {paper}\nComputer chose: {rock}" + "\nYou win!")
+        else:
+            print(f"You chose: {paper}\nComputer chose: {scissors}" + "\nYou lose!")
 
-if user_choice == 2:
-    if pc_choice == 1:
-       print(f"You chose: {scissors}\nComputer chose: {paper}" + "\nYou win!")
-    elif pc_choice == 0:
-         print(f"You chose: {scissors}\nComputer chose: {rock}" + "\nYou lose!")
-    else:
-        print(f"You chose: {scissors}\nComputer chose: {scissors}" + "\nIt's a draw!")
+    if user_choice == 2:
+        if pc_choice == 1:
+           print(f"You chose: {scissors}\nComputer chose: {paper}" + "\nYou win!")
+        elif pc_choice == 0:
+             print(f"You chose: {scissors}\nComputer chose: {rock}" + "\nYou lose!")
+        else:
+            print(f"You chose: {scissors}\nComputer chose: {scissors}" + "\nIt's a draw!")
+
+else:
+    print("You lose Cheater!")
+    
+
