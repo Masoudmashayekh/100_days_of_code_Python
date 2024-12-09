e3f6e25cbdc49a667caf55fa1f5540dfe0a47ed0
masoud mashayekh <masoudmashayekh4944@gmail.com>
2024-12-09 23:09:50 +0100

    Create flight_search.py

---
+++ Day_40_Part_2_Flight_Deal_Finder/flight_search.py
@@ -0,0 +1,78 @@
+import requests
+from datetime import datetime
+
+API_KEY = "xXx"
+API_SECRET = "xXx"
+TOKEN_ENDPOINT = "xXx"
+IATA_ENDPOINT = "xXx"
+FLIGHT_ENDPOINT = "xXx"
+
+
+class FlightSearch:
+    def __init__(self):
+        self._token = self._get_new_token()
+
+    def _get_new_token(self):
+        header = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        body = {
+            'grant_type': 'client_credentials',
+            'client_id': API_KEY,
+            'client_secret': API_SECRET
+        }
+        response = requests.post(url=TOKEN_ENDPOINT, headers=header, data=body)
+        return response.json()['access_token']
+
+    #
+    def get_destination_code(self, city_name):
+        headers = {"Authorization": f"Bearer {self._token}"}
+        query = {
+            "keyword": city_name,
+            "max": "2",
+            "include": "AIRPORTS",
+        }
+        response = requests.get(url=IATA_ENDPOINT, headers=headers, params=query)
+        data = response.json()
+        print(data["data"][0]["iataCode"])
+
+        try:
+            code = response.json()["data"][0]['iataCode']
+        except IndexError:
+            print(f"IndexError: No airport code found for {city_name}.")
+            return "N/A"
+        except KeyError:
+            print(f"KeyError: No airport code found for {city_name}.")
+            return "Not Found"
+
+        return code
+
+    def check_flights(self, origin_city_code, destination_city_code, from_time, to_time, is_direct=True):
+        headers = {"Authorization": f"Bearer {self._token}"}
+        query = {
+            "originLocationCode": origin_city_code,
+            "destinationLocationCode": destination_city_code,
+            "departureDate": from_time.strftime("%Y-%m-%d"),
+            "returnDate": to_time.strftime("%Y-%m-%d"),
+            "adults": 1,
+            "nonStop": "true" if is_direct else "false",
+            "currencyCode": "EUR",
+            "max": "10",
+        }
+
+        response = requests.get(
+            url=FLIGHT_ENDPOINT,
+            headers=headers,
+            params=query,
+        )
+
+        if response.status_code != 200:
+            print(f"check_flights() response code: {response.status_code}")
+            print("There was a problem with the flight search.\n"
+                  "For details on status codes, check the API documentation:\n"
+                  "https://developers.amadeus.com/self-service/category/flights/api-doc/flight-offers-search/api"
+                  "-reference")
+            print("Response body:", response.text)
+            return None
+
+        return response.json()
