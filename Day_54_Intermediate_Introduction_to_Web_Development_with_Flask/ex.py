27221330b195db1d650a04cb4497b16cbfdced4f
masoud mashayekh <masoudmashayekh4944@gmail.com>
2025-04-19 16:48:31 +0200

    Create ex.py

---
+++ Day_54_Intermediate_Introduction_to_Web_Development_with_Flask/ex.py
@@ -0,0 +1,28 @@
+import time
+
+
+def speed_calc_decorator(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"{func.__name__} run speed: {end_time - start_time}s")
+        return result
+
+    return wrapper
+
+
+@speed_calc_decorator
+def fast_function():
+    for i in range(1000000):
+        i * i
+
+
+@speed_calc_decorator
+def slow_function():
+    for i in range(10000000):
+        i * i
+
+
+fast_function()
+slow_function()
