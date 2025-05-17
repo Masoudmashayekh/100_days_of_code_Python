d46832f00b83ba2869c0b3a18e19b49b51f112ce
Masoud <masoudmashayekh4944@gmial.com>
2025-05-17 16:07:29 +0200

    Jinja in Flask Applications

--- Day_57_Templating_with_Jinja_in_Flask_Applications/server.py
+++ Day_57_Templating_with_Jinja_in_Flask_Applications/server.py
@@ -22,5 +22,13 @@ def guess(name):
     return render_template("guess.html", age=age, gen=gender, name= name)
 
 
+@app.route("/blog/<num>")
+def get_blog(num):
+    print(num)
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(url=blog_url)
+    all_post = response.json()
+    return render_template("blog.html", posts=all_post)
+
 if __name__ == "__main__":
     app.run(debug=True)
