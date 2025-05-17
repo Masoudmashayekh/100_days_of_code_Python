37a589cd1e2773ab823e26a5953e843b012e7990
Masoud <masoudmashayekh4944@gmial.com>
2025-05-17 18:59:28 +0200

    Jinja in Flask Applications

--- Day_57_Templating_with_Jinja_in_Flask_Applications/day-57-final project/main.py
+++ Day_57_Templating_with_Jinja_in_Flask_Applications/day-57-final project/main.py
@@ -5,8 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-
-    return render_template("index.html")
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(url=blog_url)
+    all_post = response.json()
+    return render_template("index.html",posts=all_post)
 
 if __name__ == "__main__":
     app.run(debug=True)
