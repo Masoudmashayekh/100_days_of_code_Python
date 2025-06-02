8634dd2326cef88952c9ab5b6fdd486dfdd4a55b
Masoud <masoudmashayekh4944@gmail.com>
2025-06-02 18:59:44 +0200

    change

--- Day_59_Advanced_Blog_Capstone/blog/main.py
+++ Day_59_Advanced_Blog_Capstone/blog/main.py
@@ -3,11 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return render_template("index.html",page="Home")
+    return render_template("index.html")
 
 @app.route("/about")
 def about():
-    return render_template("about.html",page="About")
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 
 if __name__ == "__main__":
