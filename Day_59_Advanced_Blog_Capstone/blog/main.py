58afdb446160ff54d7ac506ff2fb350b5d7119e5
Masoud <masoudmashayekh4944@gmail.com>
2025-06-03 14:51:27 +0200

    day 59

--- Day_59_Advanced_Blog_Capstone/blog/main.py
+++ Day_59_Advanced_Blog_Capstone/blog/main.py
@@ -1,22 +1,39 @@
 from flask import Flask, render_template
 import requests
 
-posts = requests.get("https://api.npoint.io/674f5423f73deab1e9a7").json()
+
+api = 'https://api.npoint.io/2378d3d8e0262f7e23d3'
+
+
+response = requests.get(api).json()
+
 
 app = Flask(__name__)
-@app.route("/")
+
+
+@app.route('/')
 def home():
-    return render_template("index.html")
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
+    return render_template('index.html', data=response)
 
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
+@app.route('/about')
+def click_about():
+    return render_template('about.html')
 
 
-if __name__ == "__main__":
+@app.route('/contact')
+def click_contact():
+    return render_template('contact.html')
+
+
+@app.route('/page/<int:n>')
+def goto(n):
+    cur_page = None
+    for item in response:
+        if item['id'] == n:
+            cur_page = item
+            return render_template('post.html', post=cur_page)
+
+
+if __name__ == '__main__':
     app.run(debug=True)
