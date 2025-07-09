4aade2c8b8556075385932c3ff1e2bb0e4a1ecba
Masoud <masoudmashayekh4944@gmail.com>
2025-07-09 12:04:26 +0200

    Handle POST Requests with Flask Servers

--- Day_60_Advanced_Make_POST_Requests_with_Flask_HTML_Forms/html-forms/main.py
+++ Day_60_Advanced_Make_POST_Requests_with_Flask_HTML_Forms/html-forms/main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 @app.route('/')
@@ -7,7 +8,9 @@ def home():
 
 @app.route("/login", methods=["POST"])
 def recive_data():
-    return "Success! Form submitted"
+    username = request.form["username"]
+    password = request.form["password"]
+    return f"<h1>Name: {username} with pass: {password}</h1>"
     
 if __name__  == "__main__":
     app.run(debug=True)
